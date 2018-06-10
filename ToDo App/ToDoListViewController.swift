ed76d3ce246820c293146c96a919a6d8edd58fc9
Divya Pandit Chhetri <divya@divyas-air.attlocal.net>
2018-06-10 15:08:02 -0700

    ToDo List Complete with tableview data source and delegate.

---
+++ ToDo App/ToDoListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ToDo App
+//
+//  Created by Divya Pandit Chhetri on 6/10/18.
+//  Copyright © 2018 Divya Pandit Chhetri. All rights reserved.
+//
+
+import UIKit
+
+class ToDOListViewController: UITableViewController {
+
+    let itemArray = ["Take Trash Out", "Cook Pork"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellItem", for: indexPath)
+         cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    }
+
+
+
+
